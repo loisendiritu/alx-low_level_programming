692048cf068c175e38c69d7610df018d4e17fe5d
loisendiritu <loisenndiritu@gmail.com>
2022-03-15 11:18:58 -0700

    one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - prints 
- * 
- * Description: prints ALX
- * 
+ * main - prints Holberton
+ *
+ * Description: prints Holberton
+ *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char text[8] = "_putchar"
+	char text[10] = "Holberton";
 	int i = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
